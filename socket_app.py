413e71867db3cee654687ee802a3b6ff0042d39a
MinhHieu-Nguyen-dn <mhieu.forwork@gmail.com>
2024-01-21 16:33:09 +0700

    init listening port from local server in Python to receive data from esp32

---
+++ socket_app.py
@@ -0,0 +1,41 @@
+import socket
+import json
+from whereami.pipeline import get_model
+
+
+def predict(input_string, model_path=None):
+    lp = get_model(model_path)
+    data_sample = json.loads(input_string)
+    return lp.predict(data_sample)[0]
+
+
+s = socket.socket()
+
+s.bind(('0.0.0.0', 8090))
+s.listen(0)
+
+while True:
+
+    client, addr = s.accept()
+
+    while True:
+        # content = client.recv(32).decode("utf-8")
+        data = b''
+        while True:
+            packet = client.recv(32)
+            if not packet:
+                break
+            data += packet
+
+        if len(data) == 0:
+            break
+
+        else:
+            decoded_string = str(data.decode("utf-8"))
+            decoded_string = decoded_string[:-3] + "}"
+            print(decoded_string)
+            print('Prediction: ')
+            print(predict(decoded_string))
+
+    print("Closing connection")
+    client.close()
