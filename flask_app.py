f5e0419196f748631cb81e89bd187422f41d8fd2
Hao Nguyen <nguyenquanghao1808@gmail.com>
2024-01-25 10:34:37 +0700

    update web realtime

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import json
 import socket
 import json
 from whereami.pipeline import get_model
+import threading
+
+
 
 app = Flask(__name__)
-
+socketio = SocketIO(app)
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@socketio.on('connect', namespace='/test')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect', namespace='/test')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('data')
+def handle_data(data):
     s = socket.socket()
     s.bind(('0.0.0.0', 9999))
     s.listen(0)
-
     while True:
         client, addr = s.accept()
         while True:
@@ -29,25 +45,15 @@ def home():
                 decoded_string = decoded_string[:-3] + "}"
                 print(decoded_string)
                 prediction = predict(decoded_string)
-                print('Prediction: ')
-                print(prediction)
-                return render_template_string("""
-                    <html>
-                        <body style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-                            <h1 style="font-size: 50px;">{{ prediction }}</h1>
-                        </body>
-                    </html>
-                """, prediction=prediction)
-
-        print("Closing connection")
-        client.close()
-
-
+                emit('prediction_update', {'prediction': prediction})
+                
 def predict(input_string, model_path=None):
     lp = get_model(model_path)
     data_sample = json.loads(input_string)
     return lp.predict(data_sample)[0]
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    number_thread = threading.Thread(target=predict)
+    number_thread.daemon = True
+    number_thread.start()
+    socketio.run(app, debug=True)
